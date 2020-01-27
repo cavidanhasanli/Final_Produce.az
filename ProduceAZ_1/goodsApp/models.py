2b8485b56c1005c16541497f7d3dafb5122cc1e3
narminsn <nsultanzadehh@gmail.com>
2020-01-27 17:44:00 +0400

    blog_events

--- ProduceAZ_1/goodsApp/models.py
+++ ProduceAZ_1/goodsApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
 
 
 # Create your models here.
@@ -7,10 +8,15 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255, blank=False, null=True)
-
+    slug =  models.SlugField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
 
 class Products(models.Model):
     name = models.CharField(max_length=255, blank=False, null=True)
